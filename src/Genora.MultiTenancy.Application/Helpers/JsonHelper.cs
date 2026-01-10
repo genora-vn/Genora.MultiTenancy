db6997fdb0eec9eaf45d0d6808ae0e78c2bd89cf
Tan Dang Van <tandv@baygolf.vn>
2026-01-10 09:33:21 +0700

    Fix error ExchangeCodeAsync API expires_in wrong field type

---
+++ src/Genora.MultiTenancy.Application/Helpers/JsonHelper.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json;
+
+namespace Genora.MultiTenancy.Helpers;
+
+public static class JsonHelper
+{
+    public static long ReadLongFlexible(JsonElement root, string propName, long defaultValue = 0)
+    {
+        if (!root.TryGetProperty(propName, out var el)) return defaultValue;
+
+        if (el.ValueKind == JsonValueKind.Number)
+            return el.GetInt64();
+
+        if (el.ValueKind == JsonValueKind.String)
+        {
+            var s = el.GetString();
+            if (long.TryParse(s, out var v)) return v;
+        }
+
+        return defaultValue;
+    }
+}
